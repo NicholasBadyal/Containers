7ef8956bc1d3bdac5e5a286e7c3001b8158560f8
Nicholas Badyal <nicholasbadyal@outlook.com>
2020-12-23 00:33:36 -0800

    Minimized docker cache busting and rebuilds by splitting COPY instructions

--- simple-web/server.go
+++ simple-web/server.go
@@ -7,15 +7,23 @@ import (
 	"time"
 )
 
-func indexHandler(w http.ResponseWriter, req *http.Request) {
+func index(w http.ResponseWriter, req *http.Request) {
 	_, err := io.WriteString(w, time.Now().Format("2006-01-02 15:04:05"))
 	if err != nil {
 		log.Print("failed to write")
 	}
 }
 
+func foo (w http.ResponseWriter, req *http.Request) {
+	_, err := io.WriteString(w, "foo")
+	if err != nil {
+		log.Print("failed to write")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/foo", foo)
 	err := http.ListenAndServe(":5050", nil)
 	if err != nil {
 		log.Fatal("Failed to start server: ", err)
